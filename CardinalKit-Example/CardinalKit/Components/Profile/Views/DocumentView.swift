7f630b21a03220966f814a87fcedff40a78b3ff1
var764 <97566708+var764@users.noreply.github.com>
2022-12-04 18:01:58 -0600

    Update DocumentView.swift

--- CardinalKit-Example/CardinalKit/Components/Profile/Views/DocumentView.swift
+++ CardinalKit-Example/CardinalKit/Components/Profile/Views/DocumentView.swift
@@ -8,6 +8,71 @@
 
 import SwiftUI
 import Firebase
+import CareKit
+import CareKitStore
+import UIKit
+import FirebaseFirestore
+import RealmSwift
+import ResearchKit
+import CardinalKit
+import CareKitUI
+
+struct DocumentView: View {
+
+    @State private var showPreview = false
+    var documentsURL: URL? = nil
+
+    init() {
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+        if let DocumentCollection = CKStudyUser.shared.authCollection {
+
+            // download consent document from Firebase Cloud Storage and display it to the user
+            let config = CKPropertyReader(file: "CKConfiguration")
+            let consentFileName = config.read(query: "Consent File Name")
+            let documentRef = storageRef.child("\(DocumentCollection)/\(consentFileName).pdf")
+
+            var docURL = (FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)).last as NSURL?
+            docURL = docURL?.appendingPathComponent("\(consentFileName).pdf") as NSURL?
+            let url = docURL! as URL
+            self.documentsURL = URL(fileURLWithPath: url.path, isDirectory: false)
+            UserDefaults.standard.set(url.path, forKey: "consentFormURL")
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(4), execute: {
+                let downloadTask = documentRef.write(toFile: url) { url, error in
+                    if let error = error {
+                        print("Error downloading consent document: \(error)")
+                    } else {
+                        print("Consent document downloaded successfully.")
+                    }
+                }
+            })
+            //perform(#selector(downloadDoc(documentRef, url)), with: nil, afterDelay: 4.0)
+        }
+    }
+
+    var body: some View {
+        HStack {
+            Text("View Consent Document")
+            Spacer()
+            Text("›")
+        }.frame(height: 60)
+            .contentShape(Rectangle())
+            .gesture(TapGesture().onEnded({
+                self.showPreview = true
+            }))
+            .background(DocumentPreviewViewController(self.$showPreview, url: self.documentsURL))
+    }
+}
+
+struct DDocumentView_Previews: PreviewProvider {
+    static var previews: some View {
+        DocumentView()
+    }
+}
+
+/*import SwiftUI
+import Firebase
 
 struct DocumentView: View {
     
@@ -56,6 +121,6 @@ struct DDocumentView_Previews: PreviewProvider {
     static var previews: some View {
         DocumentView()
     }
-}
+} */
 
 

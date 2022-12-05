f0b964f6e7cc042899bb67c14a2362e9cf07999d
var764 <97566708+var764@users.noreply.github.com>
2022-12-04 18:01:23 -0600

    Update WithdrawalViewController.swift

--- CardinalKit-Example/CardinalKit/Components/Profile/WithdrawalViewController.swift
+++ CardinalKit-Example/CardinalKit/Components/Profile/WithdrawalViewController.swift
@@ -57,6 +57,78 @@ struct WithdrawalViewController: UIViewControllerRepresentable {
                     if (ORKPasscodeViewController.isPasscodeStoredInKeychain()) {
                         ORKPasscodeViewController.removePasscodeFromKeychain()
                     }
+
+                    UserDefaults.standard.set(nil, forKey: Constants.prefCareKitCoreDataInitDate)
+                    UserDefaults.standard.set(nil, forKey: Constants.prefHealthRecordsLastUploaded)
+                    UserDefaults.standard.set(false, forKey: Constants.onboardingDidComplete)
+                } catch {
+                    print(error.localizedDescription)
+                    Alerts.showInfo(title: "Error", message: error.localizedDescription)
+                }
+                
+                fallthrough
+            default:
+                
+                // otherwise dismiss onboarding without proceeding.
+                taskViewController.dismiss(animated: false, completion: nil)
+                
+            }
+        }
+    }
+    
+}
+
+/*import UIKit
+import SwiftUI
+import ResearchKit
+import Firebase
+import CardinalKit
+
+struct WithdrawalViewController: UIViewControllerRepresentable {
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+
+    typealias UIViewControllerType = ORKTaskViewController
+    
+    func updateUIViewController(_ taskViewController: ORKTaskViewController, context: Context) {}
+    func makeUIViewController(context: Context) -> ORKTaskViewController {
+
+        let config = CKPropertyReader(file: "CKConfiguration")
+        
+        let instructionStep = ORKInstructionStep(identifier: "WithdrawlInstruction")
+        instructionStep.title = NSLocalizedString(config.read(query: "Withdrawal Instruction Title"), comment: "")
+        instructionStep.text = NSLocalizedString(config.read(query: "Withdrawal Instruction Text"), comment: "")
+        
+        let completionStep = ORKCompletionStep(identifier: "Withdraw")
+        completionStep.title = NSLocalizedString(config.read(query: "Withdraw Title"), comment: "")
+        completionStep.text = NSLocalizedString(config.read(query: "Withdraw Text"), comment: "")
+        
+        let withdrawTask = ORKOrderedTask(identifier: "Withdraw", steps: [instructionStep, completionStep])
+        
+        // wrap that task on a view controller
+        let taskViewController = ORKTaskViewController(task: withdrawTask, taskRun: nil)
+        
+        taskViewController.delegate = context.coordinator // enables `ORKTaskViewControllerDelegate` below
+        
+        // & present the VC!
+        return taskViewController
+
+    }
+
+    class Coordinator: NSObject, ORKTaskViewControllerDelegate {
+        public func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
+            switch reason {
+            case .completed:
+                
+                do {
+                    try CKCareKitManager.shared.wipe()
+                    try CKStudyUser.shared.signOut()
+                    
+                    if (ORKPasscodeViewController.isPasscodeStoredInKeychain()) {
+                        ORKPasscodeViewController.removePasscodeFromKeychain()
+                    }
                     
                     NotificationCenter.default.post(name: NSNotification.Name(Constants.onboardingDidComplete), object: false)
 
@@ -78,4 +150,5 @@ struct WithdrawalViewController: UIViewControllerRepresentable {
         }
     }
     
-}
+} */
+
